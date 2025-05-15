d4f0b543084d50fe8fcadb86b9502158d70bf63a
Nafa06 <naufalanggardi09@gmail.com>
2025-05-15 12:16:39 +0700

    menambahkan node newnode sebagai pembantu

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -31,5 +31,34 @@ public:
         string nm;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
+         // commit 6 menambahkan node newnode sebagai pembantu
+        // step 1: allocate memory for new node
+        Node *newNode = new Node();
+
+        // step 2: assign value to the data fields
+        newNode->noMhs = nim;
+
+        // step 3: insert at beginning if list is empty or nim is smallest
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+            // step 4: newNode.next = START
+            newNode->next = START;
+
+            // step 5: START.prev = newNode (if START exist)
+            if (START != NULL)
+                START->prev = newNode;
+
+            // step 6: newNode.prev = NULL
+            newNode->prev = NULL;
+
+            // step 7: START = newNode
+            START = newNode;
+            return;
+        }
     }
 };
