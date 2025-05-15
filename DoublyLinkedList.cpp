089677dfb9e28220a6788a7302d4528ceb1cc9e9
Nafa06 <naufalanggardi09@gmail.com>
2025-05-15 12:15:36 +0700

    menambahkan fungsi addNode

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -2,6 +2,7 @@
 #include <string>
 using namespace std;
 
+// commit 2 menambahkan class node
 class Node
 {
 public:
@@ -10,15 +11,25 @@ public:
     Node *prev;
 };
 
+// commit 3 menambahkan class doublelinkedlist
 class DoubleLinkedList
 {
 private:
     Node *START;
 
 public:
+    // commit 4 menambahkan method doublelinkedlist
     DoubleLinkedList()
     {
         START = NULL;
     }
-
+    // commit 5 menambahkan fungsi addNode
+   
+    void addNode()
+    {
+        int nim;
+        string nm;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+    }
 };
