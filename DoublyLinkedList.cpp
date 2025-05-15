77928c759bab95836201b8b654cdc015f7582528
Nafa06 <naufalanggardi09@gmail.com>
2025-05-15 12:17:54 +0700

    menambahkan fungsi hapus

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -85,4 +85,51 @@ public:
         current->next = newNode; // step 9d: current.next = newNode
         }
     }
+    // commit 8 menambahkan fungsi hapus
+    void hapus()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+        int rollNo;
+        cin >> rollNo;
+
+        // menambahkan node current sebagai pembantu
+        Node *current = START;
+
+        // step 1: traverse the list to find the node
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
+
+        // step 2: if node is at the beginning
+        if (current == START)
+        {
+            START = current->next; // step 2a: START = START.next
+            if (START != NULL)
+                START->prev = NULL; // step 2b: START.prev = NULL
+        }
+        else
+        {
+            // step 3: link previous node to next of current
+            current->prev->next = current->next;
+
+            // step 4: if current is not the last node
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+        }
+
+        // step 5: delete the node
+        delete current;
+        cout << "Record with roll number" << rollNo << " deleted" << endl;
+    }
 };
